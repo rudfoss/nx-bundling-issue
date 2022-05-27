a3f6283d9274735a42abaefd1ddc9e0e7dffe813
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2022-05-27 14:38:23 +0200

    Setup test

---
+++ packages/bundle-test/src/app/app.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import NxWelcome from './nx-welcome';
+import { MdLanguage } from "react-icons/md"
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <MdLanguage size={128}/>
+      <NxWelcome title="bundle-test" />
+    </StyledApp>
+  );
+}
+
+export default App;
